b4c19658d52d829e8829ab51e665b9d49ed156db
Jean-Francois Im <jeanfrancois.im@gmail.com>
2011-05-05 01:32:58 -0400

    Made output a bit prettier

--- src/main/scala/im/jeanfrancois/servicediscoverychallenge/client/ServiceClient.scala
+++ src/main/scala/im/jeanfrancois/servicediscoverychallenge/client/ServiceClient.scala
@@ -18,10 +18,24 @@ object ServiceClient {
     val remoteServiceRequestProcessor = new RemoteServiceRequestProcessor(remoteNode);
     remoteServiceRequestProcessor.start
 
+    // Discover remote services
     println("Sending service discovery message")
     val serviceList = (remoteServiceRequestProcessor !? ServiceRequestMessage(classOf[ServiceDiscoveryService].getName, "listServices", List[Object]())).asInstanceOf[List[String]];
-    println(serviceList)
-    serviceList.foreach(serviceclass => println(remoteServiceRequestProcessor !? ServiceRequestMessage(classOf[ServiceDiscoveryService].getName, "listServiceMethods", List[Object](serviceclass))))
+
+    // Display available services
+    println()
+    println("Available services")
+    serviceList.foreach(println);
+
+    // Display service methods
+    println()
+    println("Methods available per service")
+    serviceList.foreach(serviceclass => {
+      println(serviceclass)
+      var methodList = (remoteServiceRequestProcessor !? ServiceRequestMessage(classOf[ServiceDiscoveryService].getName, "listServiceMethods", List[Object](serviceclass))).asInstanceOf[List[String]];
+      methodList.foreach(str => println("\t" + str))
+      println()
+    })
 
     remoteServiceRequestProcessor !? Stop
   }
